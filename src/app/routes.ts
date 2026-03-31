ef8e12259f8f29589af887bc42ff9dc88878fc4b
Korven Lab <korvenlab.contato@gmail.com>
2026-03-31 09:26:01 -0300

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,6 +3,8 @@ import { HomePage } from "./App";
 import { LoginPage } from "./pages/LoginPage";
 import { Dashboard } from "./pages/Dashboard";
 import { ProtectedDashboard } from "./pages/ProtectedDashboard";
+import { PrivacyPage } from "./pages/PrivacyPage";
+import { TermsPage } from "./pages/TermsPage";
 
 export const router = createBrowserRouter([
   {
@@ -16,5 +18,14 @@ export const router = createBrowserRouter([
   {
     path: "/dashboard",
     Component: ProtectedDashboard,
+  },
+  // Novas rotas para o Google Auth
+  {
+    path: "/privacidade",
+    Component: PrivacyPage,
+  },
+  {
+    path: "/termos",
+    Component: TermsPage,
   },
 ]);
